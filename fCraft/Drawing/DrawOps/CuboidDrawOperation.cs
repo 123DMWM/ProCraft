d70e71d77230eacfc5b7c213a313c42c85cae068
123DMWM <shmo1joe2@gmail.com>
2014-10-30 16:04:12 -0400

    Small fix(?) in cuboiddrawoperation

--- fCraft/Drawing/DrawOps/CuboidDrawOperation.cs
+++ fCraft/Drawing/DrawOps/CuboidDrawOperation.cs
@@ -11,41 +11,22 @@ namespace fCraft.Drawing {
             get { return 2; }
         }
 
-        public CuboidDrawOperation( Player player )
-            : base( player ) {
-        }
+
+        public CuboidDrawOperation(Player player)
+            : base(player) { }
 
 
-        public override bool Prepare( Vector3I[] marks ) {
-            if( !base.Prepare( marks ) ) return false;
+        public override bool Prepare(Vector3I[] marks) {
+            if (!base.Prepare(marks))
+                return false;
             BlocksTotalEstimate = Bounds.Volume;
             Coords = Bounds.MinVertex;
             return true;
         }
 
 
-        public override int DrawBatch( int maxBlocksToDraw ) {
-            int blocksDone = 0;
-            for( ; Coords.X <= Bounds.XMax; Coords.X++ ) {
-                for( ; Coords.Y <= Bounds.YMax; Coords.Y++ ) {
-                    for( ; Coords.Z <= Bounds.ZMax; Coords.Z++ ) {
-                        if( !DrawOneBlock() ) continue;
-                        blocksDone++;
-                        if( blocksDone >= maxBlocksToDraw ) {
-                            Coords.Z++;
-                            return blocksDone;
-                        }
-                    }
-                    Coords.Z = Bounds.ZMin;
-                }
-                Coords.Y = Bounds.YMin;
-                if( TimeToEndBatch ) {
-                    Coords.X++;
-                    return blocksDone;
-                }
-            }
-            IsDone = true;
-            return blocksDone;
+        public override int DrawBatch(int maxBlocksToDraw) {
+            return DrawBatchWithinBounds(maxBlocksToDraw);
         }
     }
 }
